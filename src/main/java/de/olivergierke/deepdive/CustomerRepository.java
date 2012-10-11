6bfdac47ea8d49641bf9864777c3658d0f54b5e6
Oliver Gierke <info@olivergierke.de>
2012-10-11 15:23:50 +0200

    Step 5 - Redeclaring existing CRUD methods.
    Overriding findAll() to show JpaRepository actually returns a List. Overriding save(…) to customize transaction configuration.

--- src/main/java/de/olivergierke/deepdive/CustomerRepository.java
+++ src/main/java/de/olivergierke/deepdive/CustomerRepository.java
@@ -15,8 +15,11 @@
  */
 package de.olivergierke.deepdive;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * {@link Repository} to access {@link Customer} instances.
@@ -25,6 +28,19 @@ import org.springframework.data.repository.Repository;
  * @since Step 2
  */
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.CrudRepository#findAll()
+	 */
+	List<Customer> findAll();
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.CrudRepository#save(S)
+	 */
+	@Transactional(timeout = 10)
+	<S extends Customer> S save(S entity);
 
 	/**
 	 * Returns the customer with the given {@link EmailAddress}.
