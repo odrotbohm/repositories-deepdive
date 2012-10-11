6462af845c37c99b2d66776b7d3458c11632b858
Oliver Gierke <info@olivergierke.de>
2012-10-11 13:33:45 +0200

    Step 1 - Basic Spring configuration and bootstrapping test.
    Adding a basic configuration file and test cases to easily run integration tests as well as a SQL file to let Spring boot populate the in-memory database on startup.

---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+insert into Customer (id, email, firstname, lastname) values (1, 'dave@dmband.com', 'Dave', 'Matthews');
+insert into Customer (id, email, firstname, lastname) values (2, 'carter@dmband.com', 'Carter', 'Beauford');
+insert into Customer (id, email, firstname, lastname) values (3, 'boyd@dmband.com', 'Boyd', 'Tinsley');
+
+insert into Address (id, street, city, country, customer_id) values (1, '27 Broadway', 'New York', 'United States', 1);
+insert into Address (id, street, city, country, customer_id) values (2, '27 Broadway', 'New York', 'United States', 1);
