f31f08c6b55262eac2abac79b5eaccf72d8291a7
Oliver Gierke <info@olivergierke.de>
2012-10-16 14:16:21 -0400

    Step 6 - Introducing a read-only repository base interface.
    Introduced ReadOnlyRepository to capture all read-only repository methods of CrudRepository and PagingAndSortingRepository. It also fixes the the type of the ID property to Long. Added Product domain class, sample data as well as an extension of ReadOnlyRepository to declare a query method to find Product instances containing a given description.

---
+++ src/test/java/de/olivergierke/deepdive/ProductRepositoryIntegrationTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.olivergierke.deepdive;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+/**
+ * Integration tests for {@link ProductRepository}.
+ * 
+ * @author Oliver Gierke
+ * @since Step 6
+ */
+public class ProductRepositoryIntegrationTest extends AbstractIntegrationTest {
+
+	@Autowired ProductRepository repository;
+
+	/**
+	 * @since Step 6.1
+	 */
+	@Test
+	public void findsAllProducts() {
+
+		List<Product> products = repository.findAll();
+		assertThat(products, hasSize(3));
+	}
+
+	/**
+	 * @since Step 6.2
+	 */
+	@Test
+	public void findsAllAppleProductPaged() {
+
+		Page<Product> products = repository.findByDescriptionContaining("Apple", new PageRequest(0, 1));
+
+		assertThat(products.isFirst(), is(true));
+		assertThat(products.hasNext(), is(true));
+		assertThat(products, Matchers.<Product> hasItem(hasProperty("name", is("iPad"))));
+		assertThat(products, not(Matchers.<Product> hasItem(hasProperty("name", is("Dock")))));
+	}
+
+	/**
+	 * @since Step 6.3
+	 */
+	@Test
+	public void returnsOptionalEmptyForNonExistingProduct() {
+
+		Optional<Product> result = repository.findOne(4711L);
+		assertThat(result, is(Optional.empty()));
+	}
+}
